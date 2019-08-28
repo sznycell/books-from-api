106d4532045df70102ce07e7b6164833c83350f5
sznycell <51320731+sznycell@users.noreply.github.com>
2019-08-28 20:14:20 +0200

    Add files via upload

---
+++ app/models.py
@@ -0,0 +1,30 @@
+from . import db
+from datetime import datetime
+
+
+class Volumes(db.Model):
+    __tablename__ = "volumes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64))
+    authors = db.Column(db.String(64))
+    publishedDate = db.Column(db.Date)
+    industryIdentifiers = db.Column(db.Integer, unique=True)
+    pageCount = db.Column(db.Integer)
+    imageLinks = db.Column(db.String)
+    language = db.Column(db.String(64))
+
+    def to_json(self):
+        json_volumes = {
+            'title': self.title,
+            'authors': self.authors,
+            'publishedDate': self.publishedDate,
+            'industryIdentifiers': self.industryIdentifiers,
+            'pageCount': self.pageCount,
+            'imageLinks': self.imageLinks
+        }
+        return json_volumes
+
+    def __repr__(self):
+        return '<Volumes %r>' % self.title
+
---
+++ flasky.py
@@ -0,0 +1,21 @@
+import os
+from app import create_app, db
+from flask_migrate import Migrate
+from app.models import Volumes
+
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, Volumes=Volumes)
+
+
+@app.cli.command()
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)

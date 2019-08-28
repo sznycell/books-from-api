552fdfaafafa6942fccda84d0fa2cd6f4c5caf95
sznycell <51320731+sznycell@users.noreply.github.com>
2019-08-28 23:43:35 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SSL_REDIRECT = False
 
     @staticmethod
     def init_app(app):
